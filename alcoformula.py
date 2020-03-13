dbadf78eb082b3aba1f8febd8e4039bef51584e0
lopuszanski <61354513+lopuszanski@users.noreply.github.com>
2020-03-13 19:00:39 +0200

    Update

--- alcoformula.py
+++ alcoformula.py
@@ -1,29 +1,36 @@
 #Alco Tracker App v0.3
 
-ml = int(input('How much did you drink? (ml) '))
-weight = int(input('Your weight is '))
-gender = str(input('Your gender '))
-alcohol = int(input('Alcohol % '))
-ethanol = ml * (alcohol / 100)
-gendermale = round(weight * 0.7)
-genderfemale = round(weight * 0.6)
+ml = int(input('How much did you drink? (ml): '))
+weight = int(input('Your weight is: '))
+gender = str(input('Your gender (male/female): '))
+alcohol = str(input('Specify your drink (beer/wine/vodka): '))
+gendermale = weight * 0.7
+genderfemale = weight * 0.6
 
-"""
-if gender == 'male':
-    print('You are male with gender koef equal', gendermale)
-elif gender == 'female':
-    print('You are female with gender koef equal', genderfemale)
-"""
+if alcohol == 'beer':
+    alcohol = 7
+    ethanol = ml * (alcohol / 100)
+elif alcohol == 'wine':
+    alcohol = 12
+    ethanol = ml * (alcohol / 100)
+elif alcohol == 'vodka':
+    alcohol = 40
+    ethanol = ml * (alcohol / 100)
+else:
+    print('Please restart app and specify your drink.')
 
-if gender == 'male':
+if (gender == 'male') or (gender == 'man'):
     promille = ethanol / gendermale
+    sobertime = promille / 0.2
     print('Your blood alcohol =', round(promille, 2), '‰')
-elif gender == 'female':
+    print('You drunk', ethanol, 'ml clear ethanol')
+    print('You can drive approximately in', round(sobertime), 'hours')
+
+elif (gender == 'female') or (gender == 'woman'):
     promille = ethanol / genderfemale
+    sobertime = promille / 0.2
     print('Your blood alcohol =', round(promille, 2), '‰')
-
-sobertime = promille / 0.2
-
-print('You drunk', ethanol, 'ml clear ethanol')
-
-print('You can drive approximately in', round(sobertime), 'hours')
+    print('You drunk', ethanol, 'ml clear ethanol')
+    print('You can drive approximately in', round(sobertime), 'hours')
+else:
+    print('Please specify your gender.')
