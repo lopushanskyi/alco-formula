d4d467dfe228e7a7567f9ec67937ee339445a92e
lopuszanski <61354513+lopuszanski@users.noreply.github.com>
2020-03-14 22:48:57 +0200

    Added statements for male/female coefficient

--- alcoformula.swift
+++ alcoformula.swift
@@ -15,8 +15,8 @@ var currentDrink = Drink(title: "Wine", alcohol: 12, volume: 375)
 
 struct Human {
     let gender: String
-    let weight: Float
-    let coefficient: Float
+    var weight: Float
+    var coefficient: Float
     
     var weightCoef: Float {
         let weightAfterCoef = weight * coefficient
@@ -24,7 +24,14 @@ struct Human {
     }
 }
 
-let currentUser = Human(gender: "male", weight: 76, coefficient: 0.7)
+var currentUser = Human(gender: "male", weight: 76, coefficient: 0.7)
+
+if currentUser.gender == "male" {
+    currentUser.coefficient = 0.7
+}
+else {
+    currentUser.coefficient = 0.6
+}
 
 let bloodAlcoholIndex : Float = currentDrink.amountEthanol / currentUser.weightCoef
 let bloodAlcoholIndexFormatted = String(format: "%.2f", bloodAlcoholIndex)
